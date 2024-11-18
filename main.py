f2f0718b57c2140d99b0e411558a2a89abee7705
frickinbobo <andre87.weee@gmail.com>
2024-11-18 14:24:58 +0800

    restructure npm package

--- main.py
+++ main.py
@@ -18,5 +18,10 @@ def index():
   return render_template('index.html')
 
 
+@app.route('/admin/', methods=['POST', 'GET'])
+def admin():
+  return render_template('admin.html')
+
+
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
